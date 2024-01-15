287a82efa0c91547981953893c6882c47d162208
HeidiHarmony <tenibrae@gmail.com>
2024-01-15 04:06:20 -0500

    partway through a hellish rebuild where class behaviors on most elements confounded me

---
+++ assets/javascript/date.js
@@ -0,0 +1,10 @@
+function mainFunction() {
+
+    var today = document.getElementById("current-day");
+    var whatsTodayDay = dayjs().format('dddd');
+    var whatsTodayDate = dayjs().format('MMMM DD, YYYY');
+    // var myEventInput = document.getElementById('myInput');
+  
+    today.textContent = whatsTodayDay + ", " + whatsTodayDate;
+   /* today.setAttribute("style", "font-family: "); */
+}
